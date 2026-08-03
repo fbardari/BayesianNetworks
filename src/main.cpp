eb56a5240d211ef049b28af239a4ca6a4a166baf
fbardari <flaviobardari@outlook.it>
2026-08-03 14:31:01 +0200

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -4,39 +4,6 @@
 #include "Utilities.hpp"
 
 
-// test: vedo se addVariable() è in grado di capire se un network è ciclico
-void testCyclicNetwork() {
-
-    Variable v1 = {
-        "nome",
-        {"true", "false"},
-        {1},
-        {{0.5, 0.5}}
-    };
-
-    Variable v2 = {
-        "nome2",
-        {"true", "false"},
-        {0}, // network ciclico!
-        {{0.4, 0.6}, {0.5, 0.5}}
-    };
-
-    Network MyNetwork;
-
-    try
-    {
-        MyNetwork.addVariable(v1);
-        MyNetwork.addVariable(v2);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    // deve restituire errore network ciclico come atteso
-
-}
-
 int main() {
     Network network;
 
