42befeae7f6c4cc00bfdabc68da2e04c56b2d4a0
fbardari <flaviobardari@outlook.it>
2026-08-05 14:34:24 +0200

    caricato manualmente CPT di gradient.bif per test, modificato overload << per stampare matrici

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -7,10 +7,11 @@
 
 // overload dell'operatore << per fare print di vettori generici
 template <typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec) {
-    for (size_t i = 0; i < vec.size(); ++i)
-    {
-        os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<T>> &mat) {
+    for (const auto &row : mat) {
+        for (size_t j = 0; j < row.size(); ++j)
+            os << row[j] << (j + 1 == row.size() ? "" : "\t");
+        os << '\n';
     }
     return os;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,59 @@
 
 int main() {
 
-    Variable v1 = {
+    Variable a = {
         "a",
         {"true", "false"},
         {},
         {{0.5, 0.5}}
     };
 
-    Variable v2 = {
+    Variable b = {
         "b",
         {"true", "false"},
         {0},
-        {{0.5, 0.5}, {0.6, 0.4}}
+        {{0.8, 0.2},
+        {0.3, 0.7}}
     };
 
-    Variable v3 = {
+    Variable c = {
         "c",
         {"true", "false"},
-        {1},
-        {{0.4, 0.6}, {0.5, 0.5}}
+        {0},
+        {{0.6, 0.4},
+        {0.2, 0.8}}
     };
 
-    Variable v4 = {
+    Variable d = {
         "d",
         {"true", "false"},
-        {0,1},
-        {{0.4, 0.6}, {0.5, 0.5}, {0.4, 0.6}, {0.5, 0.5}}
+        {1, 2},
+        {{0.9, 0.1},
+        {0.7, 0.3},
+        {0.6, 0.4},
+        {0.1, 0.9}}
+    };
+    
+    Variable e = {
+        "e",
+        {"true", "false"},
+        {0, 2, 3},
+        {{0.95, 0.05},
+        {0.85, 0.15},
+        {0.75, 0.25},
+        {0.5, 0.5},
+        {0.8, 0.2},
+        {0.6, 0.4},
+        {0.3, 0.7},
+        {0.1, 0.9}}
     };
 
-    Network network({v1, v2, v3, v4});
+    Network network({a,b,c,d,e});
 
-    std::cout << network.getCptRow(v4, {0,0,0,0}) << "\n";
-    std::cout << network.getCptRow(v4, {0,1,0,0}) << "\n";
-    std::cout << network.getCptRow(v4, {1,0,0,0}) << "\n";
-    std::cout << network.getCptRow(v4, {1,1,0,0}) << "\n";
+    std::cout << network[network.getVariableId("a")].CPT << "\n\n";
+    std::cout << network[network.getVariableId("b")].CPT << "\n\n";
+    std::cout << network[network.getVariableId("c")].CPT << "\n\n";
+    std::cout << network[network.getVariableId("d")].CPT << "\n\n";
+    std::cout << network[network.getVariableId("e")].CPT << "\n\n";
+
 }
