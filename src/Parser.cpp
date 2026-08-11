b870d3c5813bd305c488511b28d05ad774d73f33
fbardari <flaviobardari@outlook.it>
2026-08-11 17:03:40 +0200

    costruttore e importBIF

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,1 +1,21 @@
 #include "Parser.hpp"
+
+Parser::Parser (const std::string& filename, bool log)
+    : filename(filename), log(log) {}
+
+Network Parser::importBIF(const std::string& filename, bool log) {
+    Parser parser(filename, log);
+    return parser.parse();
+}
+
+void Parser::parseNetwork() {
+
+}
+
+void Parser::parseVariable() {
+
+}
+
+void Parser::parseProbability() {
+
+}
